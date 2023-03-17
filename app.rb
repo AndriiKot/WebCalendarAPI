cbb412c6f7d4013718f932f1d2297f092213fde9
AndriiKot <a_kocyuba@ukr.net>
2023-03-17 12:33:56 +0100

    correction text

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ arg = ARGV
 p arg         # []
 
 
-# app.rb 1 2
+#ruby app.rb 1 2
 p arg  #["1","2"]
 
 a,b = ARGV[0],ARGV[1]
@@ -12,7 +12,7 @@ p (if a > b then  a else b end)   # "2"
 p a > b ? a : b                   # "2"
 
 
-# app.rb 8 44444
+#ruby app.rb 8 44444
 a,b = ARGV[0],ARGV[1]
 print a,' ', b ,"\n"              # "8" "44444"
 p (if a > b then  a else b end)   # !!! "8" !!!
@@ -21,7 +21,7 @@ p a.class    # String
 p b.class    # String
 
 
-# app.rb 8 44444
+#ruby app.rb 8 44444
 a,b = ARGV[0].to_i,ARGV[1].to_i
 print a,' ', b ,"\n"              # 8 44444
 p a.class                         #Integer
